c11b0a4098013b11481c9ed2f1cb82efefc5540a
VetVictor33 <victorjfeliciano@gmail.com>
2025-01-12 22:06:26 -0300

    Initialize Next.js project with OpenAI integration and basic layout

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "AeroBot Aldeense",
+  description: "A quickstart template using the Assistants API with OpenAI",
+  icons: {
+    icon: "/openai.svg",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+        <img className="logo" src="/openai.svg" alt="OpenAI Logo" />
+      </body>
+    </html>
+  );
+}
